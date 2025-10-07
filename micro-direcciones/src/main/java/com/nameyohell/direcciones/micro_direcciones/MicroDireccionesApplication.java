f4e9b7d7885a76ab9c8831843a47c54a23c83a21
Yohel Vasquez <yohel.vasquez@3it.cl>
2025-10-07 16:53:51 -0300

    Proyecto microservicios con Eureka y Gateway

---
+++ micro-direcciones/src/main/java/com/nameyohell/direcciones/micro_direcciones/MicroDireccionesApplication.java
@@ -0,0 +1,13 @@
+package com.nameyohell.direcciones.micro_direcciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroDireccionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroDireccionesApplication.class, args);
+	}
+
+}
---
+++ micro-direcciones/src/test/java/com/nameyohell/direcciones/micro_direcciones/MicroDireccionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nameyohell.direcciones.micro_direcciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroDireccionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
